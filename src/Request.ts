b0314791b253626db4001582885382d28719ca49
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2023-11-20 14:12:34 +0000

    chore

--- src/Request.ts
+++ src/Request.ts
@@ -1,22 +1,68 @@
-import { dual } from 'effect/Function'
-import { Body } from './internal/body.js'
-import { HttpRequest } from './internal/request.js'
+/**
+ * @since 1.0.0
+ */
+import type { Effect } from "effect/Effect";
+import { DecodeError } from "./internal/error.js";
+import * as internal from "./internal/request.js";
+import { Body } from "./internal/body.js";
 
-export const make = (url: string | URL, init?: RequestInit) => new HttpRequest(url, init)
+/**
+ * @since 1.0.0
+ * @category constructors
+ */
+export const make: (
+  url: string | URL,
+  init?: RequestInit | undefined
+) => Request = internal.make;
 
-export const map = dual<
-    <B>(fn: (request: HttpRequest) => B) => (request: HttpRequest) => B,
-    <B>(request: HttpRequest, fn: (request: HttpRequest) => B) => B
->(2, (request, fn) => fn(request))
+/**
+ * @since 1.0.0
+ * @category mapping
+ */
+export const map: {
+  <B>(fn: (request: Request) => B): (request: Request) => B;
+  <B>(request: Request, fn: (request: Request) => B): B;
+} = internal.map;
 
+/**
+ * @since 1.0.0
+ * @category mapping
+ */
+export const appendBody: (body: Body) => (request: internal.HttpRequest) => Request = internal.appendBody
 
-export const appendBody = (body: Body) => {
-    return (request: HttpRequest) => {
-        const { headers = {}, ...init } = request.init ?? {}
-        return new HttpRequest(request.url, {
-            ...init,
-            body: body.value,
-            headers: { ...headers, ...body.headers }
-        })
-    }
-}
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const json: (value: Request) => Effect<never, DecodeError, any> =
+  internal.json;
+
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const text: (value: Request) => Effect<never, DecodeError, string> =
+  internal.text;
+
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const blob: (value: Request) => Effect<never, DecodeError, Blob> =
+  internal.blob;
+
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const formData: (
+  value: Request
+) => Effect<never, DecodeError, FormData> = internal.formData;
+
+/**
+ * @since 1.0.0
+ * @category combinators
+ */
+export const arrayBuffer: (
+  value: Request
+) => Effect<never, DecodeError, ArrayBuffer> = internal.arrayBuffer;
