6b02d58d040f5dd939c847af979caf606175095a
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2023-12-11 11:06:53 +0000

    chore

--- src/Response.ts
+++ src/Response.ts
@@ -2,8 +2,22 @@
  * @since 1.0.0
  */
 import { Effect } from "effect/Effect";
+import { dual } from "effect/Function";
 import * as internal from "./internal/response/index.js";
 import { DecodeError } from "./internal/error.js";
+import { ResponseT, StatusError, StatusErrorT } from "./internal/response/index.js";
+import { NotOkStatusCode, OkStatusCode } from "./internal/response/types.js";
+import { Predicate } from "effect/Predicate";
+
+/**
+ * @since 1.0.0
+ */
+export {StatusCode, OkStatusCode, NotOkStatusCode} from './internal/response/types.js'
+
+/**
+ * @since 1.0.0
+ */
+export {StatusError} from './internal/response/index.js'
 
 /**
  * @since 1.0.0
@@ -41,3 +55,23 @@ export const formData: (
 export const arrayBuffer: (
   value: Response
 ) => Effect<never, DecodeError, ArrayBuffer> = internal.arrayBuffer;
+
+export const filterStatusOk: (
+  response: Response
+) => Effect<never, StatusError, Response> =
+  internal.filterStatusOk;
+
+export const filterStatusOkT: (
+  response: Response
+) => Effect<never, StatusErrorT<NotOkStatusCode>, ResponseT<OkStatusCode>> =
+  internal.filterStatusOkT;
+
+export const filterStatus = dual<
+  (
+    fn: Predicate<number>
+  ) => (response: Response) => Effect<never, StatusError, Response>,
+  (
+    response: Response,
+    fn: Predicate<number>
+  ) => Effect<never, StatusError, Response>
+>(2, (r, f) => internal.filterStatus(r, f));
