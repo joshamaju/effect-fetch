8faa43724a97f3da5d4753c2b18e3cf8cc24d399
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2023-12-11 11:07:47 +0000

    chore

--- example/index.ts
+++ example/index.ts
@@ -5,6 +5,7 @@ import * as Layer from "effect/Layer";
 import * as LogLevel from "effect/LogLevel";
 import * as ELogger from "effect/Logger";
 import * as Context from "effect/Context";
+import * as Match from "effect/Match";
 
 import * as Fetch from "../src/Fetch.js";
 import * as Result from "../src/Response.js";
@@ -13,7 +14,7 @@ import * as Interceptor from "../src/Interceptor.js";
 import { Level, Logger } from "../src/interceptors/Logger.js";
 
 import * as AdapterFetch from "../src/adapters/Fetch.js";
-import { Interceptor as Intercept } from "effect-fetch/internal/interceptor.js";
+import { Interceptor as Intercept } from "../src/internal/interceptor.js";
 
 const program = Effect.gen(function* (_) {
   const result = yield* _(Fetch.fetch_("/users?delay=10"));
@@ -22,6 +23,38 @@ const program = Effect.gen(function* (_) {
   console.log(users);
   // const n = yield* _(Result.filterStatusOkT(result))
 });
+
+const matchOk = Match.type<Result.OkStatusCode>().pipe(
+  Match.when(200, () => "200"),
+  Match.when(201, () => "201"),
+  Match.when(202, () => '202'),
+  Match.when(203, () => '203'),
+  Match.when(204, () => '204'),
+  Match.when(205, () => '205'),
+  Match.when(206, () => '206'),
+  Match.when(207, () => '207'),
+  Match.when(208, () => '208'),
+  Match.when(226, () => '226'),
+  Match.exhaustive
+);
+
+const matchNotOk = Match.type<Result.NotOkStatusCode>().pipe(
+  Match.when(100, () => "200"),
+  Match.when(300, () => "201"),
+  Match.when(301, () => '202'),
+  Match.when(404, () => '203'),
+  Match.when(401, () => '204'),
+  Match.when(403, () => '205'),
+  Match.when(409, () => '206'),
+  Match.when(500, () => '207'),
+  Match.orElse(() => 'noop')
+);
+
+let a = Fetch.fetch("").pipe(
+  Effect.flatMap(Result.filterStatusOkT),
+  // Effect.map((_) => matchOk(_.status)),
+  // Effect.catchTag('StatusError', _ => Effect.succeed(matchNotOk(_.response.status)))
+);
 
 interface Store {
   md(): string;
@@ -80,17 +113,14 @@ let jj = Interceptor.add(j, base_url2);
 const interceptors = pipe(
   Interceptor.empty,
   Interceptor.add(base_url),
-  Interceptor.add(base_url2),
+  Interceptor.add(base_url2)
   // Interceptor.add(
   //   Logger(Level.HEADERS, ["nel", "report-to", "reporting-endpoints"])
   // )
 );
 
 const FetchLive = Fetch.fromEffect(
-  Interceptor.makeAdapter(
-    AdapterFetch.fetch,
-    interceptors
-  )
+  Interceptor.makeAdapter(AdapterFetch.fetch, interceptors)
 );
 
 const n = pipe(
@@ -99,8 +129,8 @@ const n = pipe(
   Effect.provideService(Store, { md: () => "" }),
   Effect.tapError(Console.error),
   Effect.tapDefect(Console.error),
-  Effect.catchTag('Err', e => {
-    console.log('Err error: ', e);
+  Effect.catchTag("Err", (e) => {
+    console.log("Err error: ", e);
     return Effect.unit;
   }),
   // Effect.tapErrorCause(Console.error),
