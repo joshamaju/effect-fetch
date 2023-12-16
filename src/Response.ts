e317f9e62a56438d6bf2e2e4150d0eebf084e60c
Joshua Amaju <princejosh0007@gmail.com>
2023-12-16 15:54:48 +0100

    docs(Response)

--- src/Response.ts
+++ src/Response.ts
@@ -5,19 +5,47 @@ import { Effect } from "effect/Effect";
 import { dual } from "effect/Function";
 import * as internal from "./internal/response/index.js";
 import { DecodeError } from "./internal/error.js";
-import { ResponseT, StatusError, StatusErrorT } from "./internal/response/index.js";
+import {
+  ResponseT,
+  StatusError,
+  StatusErrorT,
+} from "./internal/response/index.js";
 import { NotOkStatusCode, OkStatusCode } from "./internal/response/types.js";
 import { Predicate } from "effect/Predicate";
 
-/**
- * @since 1.0.0
- */
-export {StatusCode, OkStatusCode, NotOkStatusCode} from './internal/response/types.js'
+export {
+  /**
+   * @since 1.1.0
+   * @category status code
+   */
+  StatusCode,
 
-/**
- * @since 1.1.0
- */
-export {StatusError, StatusErrorT} from './internal/response/index.js'
+  /**
+   * @since 1.1.0
+   * @category status code
+   */
+  OkStatusCode,
+
+  /**
+   * @since 1.0.0
+   * @category status code
+   */
+  NotOkStatusCode,
+} from "./internal/response/types.js";
+
+export {
+  /**
+   * @since 1.1.0
+   * @category error
+   */
+  StatusError,
+
+  /**
+   * @since 1.1.0
+   * @category error
+   */
+  StatusErrorT,
+} from "./internal/response/index.js";
 
 /**
  * @since 1.0.0
@@ -56,16 +84,27 @@ export const arrayBuffer: (
   value: Response
 ) => Effect<never, DecodeError, ArrayBuffer> = internal.arrayBuffer;
 
+/**
+ * @since 1.0.0
+ * @category filtering
+ */
 export const filterStatusOk: (
   response: Response
-) => Effect<never, StatusError, Response> =
-  internal.filterStatusOk;
+) => Effect<never, StatusError, Response> = internal.filterStatusOk;
 
+/**
+ * @since 1.0.0
+ * @category filtering
+ */
 export const filterStatusOkT: (
   response: Response
 ) => Effect<never, StatusErrorT<NotOkStatusCode>, ResponseT<OkStatusCode>> =
   internal.filterStatusOkT;
 
+/**
+ * @since 1.0.0
+ * @category filtering
+ */
 export const filterStatus = dual<
   (
     fn: Predicate<number>
