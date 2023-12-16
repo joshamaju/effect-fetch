065bc3dab6c1a07f37caca91c4d52591a34a7343
Joshua Amaju <princejosh0007@gmail.com>
2023-12-16 15:52:07 +0100

    docs(Error)

--- src/Error.ts
+++ src/Error.ts
@@ -1,9 +1,31 @@
 /**
  * @since 1.1.0
  */
-export { DecodeError, HttpError } from "./internal/error.js";
 
-/**
- * @since 1.1.0
- */
-export { StatusError, StatusErrorT } from "./internal/response/index.js";
+export {
+  /**
+   * @since 1.1.0
+   * @category error
+   */
+  DecodeError,
+
+  /**
+   * @since 1.1.0
+   * @category error
+   */
+  HttpError,
+} from "./internal/error.js";
+
+export {
+  /**
+   * @since 1.1.0
+   * @category error
+   */
+  StatusError,
+
+  /**
+   * @since 1.1.0
+   * @category error
+   */
+  StatusErrorT,
+} from "./internal/response/index.js";
