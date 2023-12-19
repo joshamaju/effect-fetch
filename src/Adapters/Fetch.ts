f2c6d29f0c14e55c29dff55a0709624e9c5238b9
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2023-12-19 08:33:34 +0000

    chore(docs): Fetch adapter

--- src/Adapters/Fetch.ts
+++ src/Adapters/Fetch.ts
@@ -1,13 +1,24 @@
-import * as Effect from 'effect/Effect'
-import { Fetch } from '../Fetch.js'
-import { HttpError } from '../internal/error.js'
-import { HttpRequest } from '../internal/request.js'
+/**
+ * @since 1.0.0
+ */
+
+import * as Effect from "effect/Effect";
+import { Fetch } from "../Fetch.js";
+import { HttpError } from "../internal/error.js";
+import { HttpRequest } from "../internal/request.js";
 
 const fetch_: Fetch = (url, init) => {
-    return Effect.tryPromise({
-        try: () => url instanceof HttpRequest ? fetch(url.request) : fetch(url, init),
-        catch: (error) => new HttpError(error)
-    })
-}
+  return Effect.tryPromise({
+    try: () =>
+      url instanceof HttpRequest ? fetch(url.request) : fetch(url, init),
+    catch: (error) => new HttpError(error),
+  });
+};
 
-export {fetch_ as fetch}
+export {
+  /**
+   * @since 1.0.0
+   * @category adapter
+   */
+  fetch_ as fetch,
+};
