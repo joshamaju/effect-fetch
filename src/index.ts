4ddb43edae6b2cfefa7ecd0a69f906eab21d73a2
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2024-01-03 14:02:29 +0000

    chore: add single entry barrel export

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+/**
+ * @since 1.2.4
+ */
+
+/**
+ * @since 1.2.4
+ */
+export * as Error from './Error.js'
+
+/**
+ * @since 1.2.4
+ */
+export * as Fetch from './Fetch.js'
+
+/**
+ * @since 1.2.4
+ */
+export * as Request from './Request.js'
+
+/**
+ * @since 1.2.4
+ */
+export * as Response from './Response.js'
+
+/**
+ * @since 1.2.4
+ */
+export * as Interceptor from './Interceptor.js'
+
+/**
+ * @since 1.2.4
+ */
+export * as PlatformFetch from './Adapters/Fetch.js'
+
+/**
+ * @since 1.2.4
+ */
+export * as Logger from './Interceptors/Logger.js'
