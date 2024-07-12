15e64c0ed1fb361ade672b0a06f8d5257ca5aa5c
Joshua Amaju <127889846+joshamaju@users.noreply.github.com>
2024-07-12 11:09:49 +0000

    chore: remove unnecessary flow wrapper

--- src/internal/fetch.ts
+++ src/internal/fetch.ts
@@ -1,18 +1,16 @@
 import * as Effect from "effect/Effect";
-import { flow } from "effect/Function";
 import * as Layer from "effect/Layer";
 
-import { HttpResponse } from "./response/index.js";
 import { Adapter, Fetch } from "../Fetch.js";
+import { HttpResponse } from "./response/index.js";
 
 export const fetch = (url: string | URL, init?: RequestInit) => {
-  return Effect.flatMap(Fetch, (fetch_) => fetch_(url, init));
+  return Effect.flatMap(Fetch, (fetch) => fetch(url, init));
 };
 
-export const fetch_ = flow(
-  fetch,
-  Effect.map((response) => new HttpResponse(response))
-);
+export const fetch_ = (url: string | URL, init?: RequestInit) => {
+  return Effect.map(fetch(url, init), (res) => new HttpResponse(res));
+};
 
 export const make = (fetch: Adapter) => Layer.succeed(Fetch, fetch);
 
